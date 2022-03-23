9203690cefaab11837410a25e4fdc84823f9b4e2
Charya Sendanayake <charyasendanayake3@gmail.com>
2022-03-24 06:17:54 +0800

    More zeitwerk

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ Bundler.require(*Rails.groups)
 module SampleProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 6.0
 
     # Configuration for the application, engines, and railties goes here.
     #
