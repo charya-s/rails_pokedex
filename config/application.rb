2e669244c29ed8214e331345eea91dbbd0b041cb
Charya Sendanayake <charyasendanayake3@gmail.com>
2022-03-24 06:06:30 +0800

    Getting rid of zeitwerk
    config.autoloader = :classic

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,7 @@ module SampleProject
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoloader = :classic
     config.app_generators.scaffold_controller = :scaffold_controller
 
     # Middleware for ActiveAdmin
