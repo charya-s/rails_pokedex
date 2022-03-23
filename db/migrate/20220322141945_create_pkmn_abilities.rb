b2a3ba3151e9cf2991b36a5fc7b4d069cfa2c20b
Charya Sendanayake <charyasendanayake3@gmail.com>
2022-03-24 05:51:16 +0800

    Fixed pkmn_abilities migration
    Removed unnecessary reference to :pkmn

--- db/migrate/20220322141945_create_pkmn_abilities.rb
+++ db/migrate/20220322141945_create_pkmn_abilities.rb
@@ -1,7 +1,7 @@
 class CreatePkmnAbilities < ActiveRecord::Migration[7.0]
   def change
     create_table :pkmn_abilities do |t|
-      t.references :pkmn, null: false, foreign_key: true
+      t.integer :pkmn_general_id
       t.integer :abilities_num
       t.string :ability_1
       t.string :ability_2
